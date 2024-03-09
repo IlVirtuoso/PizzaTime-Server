3d9be9cfb3c142ab7a77c322b8a29c1d0d4b29b3
DrFaust <matteo.ielacqua@edu.unito.it>
2024-03-10 00:46:20 +0100

    add navbar

--- PizzaTimeSPA/src/App.vue
+++ PizzaTimeSPA/src/App.vue
@@ -6,30 +6,27 @@ import TopMenu  from "./components/TopMenu.vue"
 </script>
 
 <template>
-  <TopMenu />
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
+    <div class="common-layout">
+    <el-container>
+      <el-header class="navbar">
+        <TopMenu />
+      </el-header>
+
+      <el-main>
+        <!-- Here goes visualizer -->
   <UserSignForm />
   <PizzeriaSignForm />
+      </el-main>
+
+      <el-footer>
+
+      </el-footer>
+    </el-container>
+  </div>
+  
+
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #8183a6aa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>./components/Login.vue./components/UserSignForm.vue
+
+</style>
--- PizzaTimeSPA/src/components/TopMenu.vue
+++ PizzaTimeSPA/src/components/TopMenu.vue
@@ -1,25 +1,44 @@
 <script setup lang="ts">
 import { ref } from 'vue';
 
+const index = defineModel('index')
+
+const menu = ref();
+
+
+function change(key: string, keypath : string[]){
+  index.value = key;
+}
 
 </script>
 
 
 <template>
-<el-menu :default-active="'1'" class="" mode="horizontal">
+<el-menu :default-active="'1'" class="navbar el-menu" mode="horizontal" text-color="#2f3640" active-text-color="#409eff"  :model="menu" @select="change">
+    <el-menu-item index="0">
+      <p>logo here</p>
+    </el-menu-item>
   <el-menu-item index="1">Menu1</el-menu-item>
-  <el-submenu index="2">
+  <el-sub-menu index="2">
     <template #title>Menu2</template>
-    <el-menu-item index="2-1">SubItem</el-menu-item>
-    <el-menu-item index="2-2">SubItem</el-menu-item>
-    <el-menu-item index="2-3">SubItem</el-menu-item>
-  </el-submenu>
-  <el-menu-item index="3" disabled>Disabled</el-menu-item>
+    <el-menu-item index="2-1">Item 1</el-menu-item>
+    <el-menu-item index="2-2">Item 2</el-menu-item>
+  </el-sub-menu>
 </el-menu>
 </template>
 
 <style scoped>
 .read-the-docs {
     color: #888;
+}
+.navbar {
+  background: transparent;
+  border-bottom: solid 1px #2f3640;
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  z-index: 999;
+
 }
 </style>
