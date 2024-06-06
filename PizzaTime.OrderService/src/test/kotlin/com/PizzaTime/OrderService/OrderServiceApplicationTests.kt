f77b534c081a80995c595648e428e51ea5a278de
DrFaust <matteo.ielacqua@edu.unito.it>
2024-06-06 16:36:10 +0200

    add order microservice

---
+++ PizzaTime.OrderService/src/test/kotlin/com/PizzaTime/OrderService/OrderServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.PizzaTime.OrderService
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class OrderServiceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
